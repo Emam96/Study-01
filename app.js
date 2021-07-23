4396bcac3f4ceddaed77828a9b46bf69a617d1e4
Emam96 <emamsh990@gmail.com>
2021-07-23 20:00:36 +0300

    done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let certi = document.getElementById('card'); 
 let options = document.querySelector('form'); 
 let show = document.getElementById('certife'); 
+let btn = document.getElementById('BTn'); 
 let users = [];
 
 
@@ -13,33 +14,32 @@ users.push(this);
 }
 
 
-
-function pushToLocalStorage() {
-   
-   let codedData = JSON.stringify(users); 
-   localStorage.setItem("data", codedData);
-  }
-
-
-  
-  function retryFromLocalStorage() {
+function retryFromLocalStorage() {
     let retryData = localStorage.getItem("data");
-    let decodedData = JSON.parse(retryData);
-    if (decodedData !== null) {
-      for (let i = 0; i < decodedData.length; i++) {
-        users[i].name = decodedData[i].name;
-        users[i].vacc = decodedData[i].vacc;
+    
+    if (retryData !== null) {
+        let decodedData = JSON.parse(retryData);
+      users = decodedData
        
       }
 
      
 
     }
-  }
+  
+
+  retryFromLocalStorage();
 
 
 
+  
+  
 
+  function pushToLocalStorage() {
+   
+    let codedData = JSON.stringify(users); 
+    localStorage.setItem("data", codedData);
+   }
 
 
 
@@ -53,16 +53,18 @@ function showCerti(event) {
     let title =  event.target[1].value;
    let vacType =  event.target[2].value;
    new User(title, vacType);
+   render();
   pushToLocalStorage();
-
-
-console.log(users);
-//   retryFromLocalStorage();
+}
 
 
 
+function render () {
+
+    show.innerHTML = '';
+    for (let i = 0; i < users.length; i++) {
   let h2Elm = document.createElement('h2'); 
-  h2Elm.textContent = `He/She (${users[0].name}) got the damn (${users[0].vacc}) vaccine`
+  h2Elm.textContent = `He/She (${users[i].name}) got the damn (${users[i].vacc}) vaccine`
   show.appendChild(h2Elm);
   let spanElm = document.createElement('span'); 
   spanElm.textContent = 'pass this dude'
@@ -72,10 +74,25 @@ console.log(users);
   stamp.setAttribute("src", 'https://media.istockphoto.com/vectors/vaccinated-covid19-red-stamp-corona-virus-vaccination-rubber-stamp-vector-id1308089702?k=6&m=1308089702&s=612x612&w=0&h=BOvKvDrAgOLzwmw26u7yd5ax5Jhd-CZglXwFTqBB56Y=');
   stamp.setAttribute("width", "200px");
   show.appendChild(stamp);
-  options.removeEventListener('submit', showCerti);
+  let hrElm = document.createElement('hr'); 
+  hrElm.setAttribute('class', "hoi")
+  show.appendChild(hrElm);
+}
+//   options.removeEventListener('submit', showCerti);
+
 
 }
 
 
+render();
 
 
+btn.addEventListener('click', deleteData)
+
+function deleteData(event){
+event.preventDefault();
+    localStorage.clear();
+    location.href = "index.html";
+   
+    
+}
